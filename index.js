2f3c69a72e74210a643b6547d4cb29247249b917
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-04-05 13:16:39 -0300

    [FABOSSI] Gamer 100%

--- index.js
+++ index.js
@@ -3,26 +3,35 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function () {
-            this.monsterHealth -= this.calculateDamage(3, 10);
-
-            if (this.checkWin()) {
-                return;
-            }
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for ' + damage
+            })
+            if (this.checkWin()) { return };
 
             this.monsterAttack();
         },
 
         monsterAttack: function () {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for ' + damage
+            })
             this.checkWin();
         },
         specialAttack: function () {
